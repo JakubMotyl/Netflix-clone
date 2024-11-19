0b7b2f638ab9181df3ad3b777fdfa61399c0ab23
Jakub Motyl <kubamotyl16@gmail.com>
2024-11-19 15:19:26 +0100

    Repair Code

--- src/components/data/Requests.js
+++ src/components/data/Requests.js
@@ -1,4 +1,4 @@
-const API_KEY = 'd9e0005a61abc6e06615d9107159d093';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const requests = {
   fetchNetflixPopularMovies: `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
@@ -8,7 +8,7 @@ const requests = {
   fetchNetflixDocumentaryMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99`,
   fetchNetflixRomanceMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`,
   fetchNetflixFamilyMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10751`,
-  fetchNetflixMusicMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10402`
+  fetchNetflixMusicMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10402`,
 };
 
 export default requests;
